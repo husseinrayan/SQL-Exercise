794a3dfc135da1c617afd5c3f85811b25f6db9e1
rayan <rayanfayadhussein98@gmail.com>
2022-12-12 19:33:46 +0200

    Creating Table

--- answers.sh
+++ answers.sh
@@ -5,3 +5,18 @@ SELECT Points FROM students WHERE name="Alex";
 INSERT INTO students(name, Age,Gender,Points) VALUES ("RAYAN" ,"24" ,"F", "1000");
 UPDATE students SET Points="400" WHERE name="Basma";
 UPDATE students SET Points="110" WHERE name="Alex";
+### Creating Table
+CREATE TABLE "graduated" (
+        "ID"    INTEGER NOT NULL,
+        "Name"  TEXT NOT NULL UNIQUE,
+        "Age"   INTEGER,
+        "Gender"        TEXT,
+        "Points"        INTEGER,
+        "Graduation"    TEXT,
+        PRIMARY KEY("ID" AUTOINCREMENT)
+)
+INSERT INTO graduated (name,Age,Gender,Points) SELECT name,Age,gender,Points FROM students WHERE name="Layal";
+
+UPDATE graduated SET Graduation= "08/09/2018" WHERE name="Layal";
+
+DELETE FROM students WHERE name="Layal";
